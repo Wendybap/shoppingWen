a6df30cdf6767270119d08c6399e00b02712ce52
wendy Arcia <wendy_342@hotmail.com>
2023-08-16 18:02:41 -0300

    feat: rendering the orders list in the My Orders component

--- src/Pages/MyOrder/index.jsx
+++ src/Pages/MyOrder/index.jsx
@@ -1,7 +1,9 @@
 import { useContext } from "react";
+import { Link } from "react-router-dom";
 import { ShoppingCartContext } from "../../Context";
 import Layout from "../../Components/Layout";
 import OrderCard from "../../Components/OrderCard";
+import { ChevronDoubleLeftIcon } from "@heroicons/react/24/solid";
 
 function MyOrder() {
   const context = useContext(ShoppingCartContext);
@@ -9,7 +11,13 @@ function MyOrder() {
   return (
     <>
       <Layout>
-        My Order
+        <div className="flex w-80 items-center justify-center relative mb-6">
+          <Link to={"/MyOrders"} className="absolute left-0">
+            <ChevronDoubleLeftIcon className="h-6 w-6 text-black-500 cursor-pointer " />
+          </Link>
+          <h1>My Order</h1>
+        </div>
+
         <div className="flex flex-col w-80">
           {context.order?.slice(-1)[0].products.map((product) => (
             <OrderCard
--- src/Pages/MyOrders/index.jsx
+++ src/Pages/MyOrders/index.jsx
@@ -1,9 +1,26 @@
+import { useContext } from "react";
+import { ShoppingCartContext } from "../../Context";
 import Layout from "../../Components/Layout";
+import OrdersCard from "../../Components/OrdersCard";
+import { Link } from "react-router-dom";
 
 function MyOrders() {
+  const context = useContext(ShoppingCartContext);
   return (
     <>
-      <Layout>My Orders</Layout>
+      <Layout>
+        <div className="flex w-80 items-center justify-center relative">
+          <h1>My Orders</h1>
+        </div>
+        {context.order.map((order, index) => {
+          <Link key={index} to={`/MyOrders/${order.id}`}>
+            <OrdersCard
+              totalPrice={order.totalPrice}
+              totalProducts={order.totalProducts}
+            />
+          </Link>;
+        })}
+      </Layout>
     </>
   );
 }
